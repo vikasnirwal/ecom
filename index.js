5a9c4b2786f822f4c840d05fdd16726f70260ecd
vikasnirwal <vikaskumarnirwal123@gmail.com>
2017-01-22 12:58:53 +0530

    vikas nirwal

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+
+app.get('/',function(req,rec){
+	rec.json("hiiiii");
+});
 app.listen(3000,function(err){
 	if(err) throw err;
 	
-	console.log("ddddddddddddddddddddd");
+	console.log("Server is running at port 3000");
 });
